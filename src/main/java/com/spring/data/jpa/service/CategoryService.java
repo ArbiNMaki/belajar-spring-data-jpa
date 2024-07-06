9bb61242aa182a3c390efeefd2ca7f678c727044
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-07-07 05:56:50 +0700

    Programmatic Transaction

--- src/main/java/com/spring/data/jpa/service/CategoryService.java
+++ src/main/java/com/spring/data/jpa/service/CategoryService.java
@@ -4,13 +4,24 @@ import com.spring.data.jpa.entity.Category;
 import com.spring.data.jpa.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.transaction.support.TransactionOperations;
 
 @Service
 public class CategoryService {
 
     @Autowired
     private CategoryRepository categoryRepository;
+
+    @Autowired
+    private TransactionOperations transactionOperations;
+
+    @Autowired
+    private PlatformTransactionManager transactionManager;
 
     @Transactional
     public void create() {
@@ -25,5 +36,44 @@ public class CategoryService {
 
     public void test() {
         create();
+    }
+
+    public void error() {
+        throw new RuntimeException("Ups");
+    }
+
+    public void createCategories() {
+        transactionOperations.executeWithoutResult(transactionStatus -> {
+            for (int i = 0; i < 5; i++) {
+                Category category = new Category();
+                category.setName("Category " + i);
+                categoryRepository.save(category);
+            }
+
+            error();
+        });
+    }
+
+    public void manual() {
+        DefaultTransactionDefinition definition = new DefaultTransactionDefinition();
+        definition.setTimeout(10);
+        definition.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+
+        TransactionStatus transaction = transactionManager.getTransaction(definition);
+
+        try {
+            for (int i = 0; i < 5; i++) {
+                Category category = new Category();
+                category.setName("Category Manual " + i);
+                categoryRepository.save(category);
+            }
+
+            error();
+
+            transactionManager.commit(transaction);
+        } catch (Throwable throwable) {
+            transactionManager.rollback(transaction);
+            throw throwable;
+        }
     }
 }
