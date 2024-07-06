8854c17ae93e4ae1d219c93a59d657e86797c2e9
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-07-07 06:56:38 +0700

    Query Relation

---
+++ src/test/java/com/spring/data/jpa/repository/ProductRepositoryTest.java
@@ -0,0 +1,53 @@
+package com.spring.data.jpa.repository;
+
+import com.spring.data.jpa.entity.Category;
+import com.spring.data.jpa.entity.Product;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class ProductRepositoryTest {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Test
+    void createProduct() {
+        Category category = categoryRepository.findById(4L).orElse(null);
+        assertNotNull(category);
+
+        {
+            Product product = new Product();
+            product.setName("Apple Iphone 14 Pro Max");
+            product.setPrice(25_000_000L);
+            product.setCategory(category);
+            productRepository.save(product);
+        }
+        {
+            Product product = new Product();
+            product.setName("Apple Iphone 15 Pro Max");
+            product.setPrice(35_000_000L);
+            product.setCategory(category);
+            productRepository.save(product);
+        }
+    }
+
+    @Test
+    void findProducts() {
+        List<Product> products = productRepository
+                .findAllByCategory_Name("GADGET MURAH");
+
+        assertEquals(2, products.size());
+        assertEquals("Apple Iphone 14 Pro Max", products.get(0).getName());
+        assertEquals("Apple Iphone 15 Pro Max", products.get(1).getName());
+    }
+}
