f365f558d5aaec060e283562261cf2bb9f660cd9
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-07-07 07:28:18 +0700

    Paging

--- src/test/java/com/spring/data/jpa/repository/ProductRepositoryTest.java
+++ src/test/java/com/spring/data/jpa/repository/ProductRepositoryTest.java
@@ -5,6 +5,8 @@ import com.spring.data.jpa.entity.Product;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 
 import java.util.List;
@@ -60,5 +62,24 @@ class ProductRepositoryTest {
         assertEquals(2, products.size());
         assertEquals("Apple Iphone 15 Pro Max", products.get(0).getName());
         assertEquals("Apple Iphone 14 Pro Max", products.get(1).getName());
+    }
+
+    @Test
+    void findProductsWithPageable() {
+        // Page 0
+        Pageable pageable = PageRequest.of
+                (0, 1, Sort.by(Sort.Order.desc("id")));
+        List<Product> products = productRepository.findAllByCategory_Name("GADGET MURAH", pageable);
+
+        assertEquals(1, products.size());
+        assertEquals("Apple Iphone 15 Pro Max", products.get(0).getName());
+
+        // Page 1
+        pageable = PageRequest.of
+                (1, 1, Sort.by(Sort.Order.desc("id")));
+        products = productRepository.findAllByCategory_Name("GADGET MURAH", pageable);
+
+        assertEquals(1, products.size());
+        assertEquals("Apple Iphone 14 Pro Max", products.get(0).getName());
     }
 }
